06cafba40141ddc3921ac13a0d4ea37f0718b1d1
Viacheslav Tykhovlis <front.tykhovlis@gmail.com>
2022-11-13 22:27:12 +0100

    step4

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import css from './App.module.css';
-import { nanoid } from 'nanoid';
+import { ContactForm } from './ContactForm/ContactForm';
+import { Filter } from './Filter/Filter';
+import { ContactList } from './ContactList/ContactList';
+import { Contact } from './Contact/Contact';
 
 export class App extends React.Component {
   state = {
@@ -11,36 +14,12 @@ export class App extends React.Component {
       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
     filter: '',
-    name: '',
-    number: '',
-  };
-  // userId = nanoid(10);
-
-  getName = evt => {
-    this.setState({
-      name: evt.target.value,
-    });
   };
 
-  getNumber = evt => {
-    this.setState({
-      number: evt.target.value,
-    });
-  };
-
-  addContact = evt => {
-    evt.preventDefault();
+  handleSubmit = contact => {
     this.setState(prevState => ({
-      contacts: [
-        ...prevState.contacts,
-        { name: this.state.name, id: nanoid(10), number: this.state.number },
-      ],
+      contacts: [...prevState.contacts, contact],
     }));
-    this.resetName();
-  };
-
-  resetName = () => {
-    this.setState({ name: '' });
   };
 
   changeFilter = evt => {
@@ -57,58 +36,16 @@ export class App extends React.Component {
     return (
       <div className={css.mainDiv}>
         <h2>Phonebook</h2>
-        <form className={css.formInput} onSubmit={this.addContact}>
-          <label className={css.labelName}>
-            Name:
-            <br />
-            <input
-              className={css.inputName}
-              type="text"
-              name="name"
-              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-              required
-              onChange={this.getName}
-            />
-          </label>
-          <label className={css.labelName}>
-            Number:
-            <br />
-            <input
-              className={css.inputName}
-              type="tel"
-              name="number"
-              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-              required
-              onChange={this.getNumber}
-            />
-          </label>
-          <button type="submit" className={css.btnContact}>
-            Add contact
-          </button>
-        </form>
+
+        <ContactForm onSubmit={this.handleSubmit} />
 
         <h2>Contacts</h2>
-        <label>
-          Find contact by name <br />
-          <input
-            type="text"
-            value={this.state.filter}
-            onChange={this.changeFilter}
-          />
-        </label>
 
-        <ul>
-          {filtredContacts.map(contact => {
-            return (
-              <li key={contact.id} className={css.contactItem}>
-                <p className={css.name}>{contact.name}</p>
-                <span className={css.number}>{contact.number}</span>
-              </li>
-            );
-          })}
-        </ul>
+        <Filter onChange={this.changeFilter} value={this.state.filter} />
+
+        <ContactList>
+          <Contact contactList={filtredContacts} />
+        </ContactList>
       </div>
     );
   }
