f6f92735f2a535ed5ab239ac357abb1cd042bfee
Viacheslav Tykhovlis <front.tykhovlis@gmail.com>
2022-11-13 11:53:19 +0100

    step1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,65 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React from 'react';
+import css from './App.module.css';
+import { nanoid } from 'nanoid';
+
+export class App extends React.Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+  // userId = nanoid(10);
+
+  getName = evt => {
+    this.setState({
+      name: evt.target.value,
+    });
+  };
+
+  addContact = evt => {
+    evt.preventDefault();
+    this.setState(prevState => ({
+      contacts: [
+        ...prevState.contacts,
+        { name: this.state.name, id: nanoid(10) },
+      ],
+    }));
+    this.resetName();
+  };
+
+  resetName = () => {
+    this.setState({ name: '' });
+  };
+
+  render() {
+    return (
+      <div className={css.mainDiv}>
+        <h2>Phonebook</h2>
+        <form className={css.formInput} onSubmit={this.addContact}>
+          <label className={css.labelName}>
+            Name:
+            <br />
+            <input
+              className={css.inputName}
+              type="text"
+              name="name"
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required
+              onChange={this.getName}
+            />
+          </label>
+          <button type="submit" className={css.btnContact}>
+            Add contact
+          </button>
+        </form>
+
+        <h2>Contacts</h2>
+        <ul>
+          {this.state.contacts.map(contact => {
+            return <li key={contact.id}>{contact.name}</li>;
+          })}
+        </ul>
+      </div>
+    );
+  }
+}
